9c7582dec21bb2d6dcebc332d4841762aa95f921
Rogher Soares <rogher_adriano@hotmail.com>
2025-05-13 21:45:31 -0300

    validando o segundo digito verificador

--- script.js
+++ script.js
@@ -22,4 +22,21 @@ function validarCPF (cpf){
     if (resto !== parseInt(cpf.substring(9, 10))) {
         return false;
     }
+
+    // Validação do segundo dígito verificador
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
 }
