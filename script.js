bee6d9842e6e5455291ea8f97d0a04e14833cf43
Rogher Soares <rogher_adriano@hotmail.com>
2025-05-13 21:10:53 -0300

    verificando se o cpf e valido

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function validarCPF (cpf){
     cpf = cpf.replace(/[^\d]+/g,'');
+    
+    // Verifica se o CPF tem 11 dígitos e não é uma sequência de números iguais
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+        return false;
+    }
+    
 }
